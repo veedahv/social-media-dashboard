e3a75e5ae19a0deb327002b56e9179217afaf915
veedahv <veedahv55@gmail.com>
2020-09-20 16:18:08 +0100

    new commit

---
+++ js/style.js
@@ -0,0 +1,14 @@
+
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+  }
+
+// function toggle(checkbox)
+// {
+//   if(document.getElementById("check-box").value=="OFF"){
+//    document.getElementById("check-box").value="ON";}
+
+//   if(document.getElementById("check-box").value=="ON"){
+//    document.getElementById("check-box").value="OFF";}
+// }
