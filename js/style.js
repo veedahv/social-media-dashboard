2fe84064043ae4d06dce8689dce9b1c75c46e840
veedahv <veedahv55@gmail.com>
2020-12-19 12:53:22 +0100

    new commit

--- js/style.js
+++ js/style.js
@@ -1,5 +1,17 @@
 
-function myFunction() {
-    var element = document.body;
-    element.classList.toggle("dark-mode");
+
+const toggleMode = document.querySelector('.toggle-mode');
+const switchMode = document.querySelector('.switch');
+
+
+
+toggleMode.addEventListener('change', () => {
+  const element = document.body;
+  if (toggleMode.checked) {
+    element.classList.add("dark-mode");
+    switchMode.classList.add("active");
+  } else {    
+    element.classList.remove("dark-mode");
+    switchMode.classList.remove("active");
   }
+})
